deb2572b9042742446123582a05cd2146dba99b5
Hiruni <hirunid4216@gmail.com>
2026-09-19 01:21:09 +0530

    update hospital dataheader declaration

--- hospital_data.h
+++ hospital_data.h
@@ -5,6 +5,17 @@
 #define NUM_WARDS 4
 #define MAX_BEDS 20
 
+extern const int specialtyID[NUM_SPECIALTIES];
+extern const char specialtyName[NUM_SPECIALTIES][30];
+extern const double consultationFee[NUM_SPECIALTIES];
+extern const int consultationTime[NUM_SPECIALTIES];
+extern const int dailyPatientCap[NUM_SPECIALTIES];
+
+extern const int wardID[NUM_WARDS];
+extern const char wardName[NUM_WARDS][30];
+extern const double dailyBedRate[NUM_WARDS];
+extern const int bedCapacity[NUM_WARDS];
+
 extern int bedOccupancy[NUM_WARDS][MAX_BEDS];
 
 void displaySpecialties(void);
